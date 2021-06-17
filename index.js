0fa77cfedcaea6540a6256a7f1dec22eb98d2117
bz.yang <bz.yang@kkday.com>
2021-06-17 17:08:24 +0800

    update

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
+const context = github.context;
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
+    let env
+    if (context.eventName === 'push') {
+      env = context.ref.endsWith('master') ? 'sta' : context.ref.includes('stable/') ? 'uat' : context.ref.includes('feature/') ? 'sit' : null
+    } else if (context.eventName === 'release') {
+      env = 'prod'
+    }
+    if (env) {
+      core.info('environment is ' + env)
+    } else {
+      core.info('unknown environment!')
+    }
+    
     core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    core.exportVariable('REZIO_ENV', env)
+    core.setOutput('rezio-env', env);
   } catch (error) {
     core.setFailed(error.message);
   }
